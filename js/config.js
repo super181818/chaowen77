10963584d22017c0b9385c5a8c98b8f54986cd93
super181818 <131360198+super181818@users.noreply.github.com>
2023-11-28 19:29:40 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -7,7 +7,6 @@ var BaseUrl = 'http://' + _wip+':'+_port;
 var ResourceUrl = '/';
 var Suffix = '.html';
 var _isTx = 1;
-var _wport = '10000';
-
+var _wport = '10001';
 
 /*document.write('<script type="text/javascript" src="http://' + _wip + ':1999/cwoss.js?v=' + _tt + '"> <\/script>');*/
